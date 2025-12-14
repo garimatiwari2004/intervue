b2b20229fe091ec1ab465749f219f98ba5e10384
Garima Tiwari <garimatiwari1803@gmail.com>
2025-12-14 16:12:42 +0530

    add console log for socket URL during initialization

--- frontend/frontend/src/socket.js
+++ frontend/frontend/src/socket.js
@@ -1,8 +1,10 @@
 import { io } from "socket.io-client";
 
+console.log("SOCKET URL:", import.meta.env.VITE_SOCKET_URL);
+
 export const socket = io(
   import.meta.env.VITE_SOCKET_URL,
   {
-    transports: ["websocket"]
+    transports: ["websocket"],
   }
 );
